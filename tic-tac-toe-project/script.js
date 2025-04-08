a44a538d9aaa15ffbcc00ae3bc50e31dc964515a
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-04-08 15:42:43 -0500

    completed logic to create gameboard

--- tic-tac-toe-project/script.js
+++ tic-tac-toe-project/script.js
@@ -1,4 +1,30 @@
-const body = document.querySelector("body");
-const button = document.createElement("p");
-button.textContent = "Hello, world!";
-body.appendChild(button);
+const gameBoard = (function () {
+  const boardArray = []; // array that will hold game state
+
+  // cache DOM
+  const board = document.querySelector(".board");
+
+  // function that creates square div elements for board and attaches an event listenter to each square
+  function createBoard() {
+    for (let i = 0; i < 9; i++) {
+      const square = document.createElement("div");
+
+      square.classList.add("square");
+      square.setAttribute("id", `${i}`);
+
+      // TODO: add functionality per click
+      square.addEventListener("click", () => {
+        square.textContent = "X";
+        console.log(`Clicked square ${i}`);
+      });
+
+      board.appendChild(square);
+    }
+  }
+
+  return {
+    boardArray,
+    createBoard,
+  };
+})();
+
