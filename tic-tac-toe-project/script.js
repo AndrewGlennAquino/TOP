09ed83d65048846415fe9fa0433c9d34604bd126
AndrewGlennAquino <andrewglenn.aquino@gmail.com>
2025-04-08 17:00:28 -0500

    added start and restart button functionality

--- tic-tac-toe-project/script.js
+++ tic-tac-toe-project/script.js
@@ -1,10 +1,13 @@
+/**
+ * Contains the array representation of the game board, creates a new game board, and clears the game board.
+ */
 const gameBoard = (function () {
-  const boardArray = []; // array that will hold game state
+  const boardArray = []; // Array that will hold the state of the game
 
-  // cache DOM
+  // DOM elements
   const board = document.querySelector(".board");
 
-  // function that creates square div elements for board and attaches an event listenter to each square
+  // Function that creates square div elements for board and attaches an event listenter to each square
   function createBoard() {
     for (let i = 0; i < 9; i++) {
       const square = document.createElement("div");
@@ -22,9 +25,40 @@ const gameBoard = (function () {
     }
   }
 
+  // Function that clears the board and boardArray
+  function clearBoard() {
+    for (let i = 0; i < 9; i++) {
+      const square = document.getElementById(`${i}`);
+      square.remove();
+    }
+
+    boardArray = [];
+  }
+
   return {
     boardArray,
     createBoard,
+    clearBoard,
   };
 })();
 
+/**
+ * TODO: implement game logic
+ */
+const gameLogic = (function(boardArray) {
+
+  return {};
+})(gameBoard.boardArray);
+
+/**
+ * TODO: implement the game
+ */
+const game = (function() {
+  // DOM elements
+  const startButton = document.getElementById("start-button");
+  const restartButton = document.getElementById("restart-button");
+
+  // Add functionality to start and stop buttons
+  startButton.addEventListener("click", () => gameBoard.createBoard());
+  restartButton.addEventListener("click", () => gameBoard.clearBoard());
+})();
