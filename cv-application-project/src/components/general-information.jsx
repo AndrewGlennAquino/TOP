51d89db69e299bd495105e97524e557c02794c1b
Andrew Aquino <andrew.aquino20@icloud.com>
2025-05-17 23:16:11 -0500

    implemented event listeners to general-information

--- cv-application-project/src/components/general-information.jsx
+++ cv-application-project/src/components/general-information.jsx
@@ -6,25 +6,38 @@ function GeneralInformation() {
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="general-information-container">
       <form>
         <div>
           <label>Name: </label>
-          <input type="text" className="input-box"></input>
+          <input
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+          ></input>
         </div>
 
         <div>
           <label>Email: </label>
-          <input type="text" className="input-box"></input>
+          <input
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
         </div>
 
         <div>
           <label>Phone number: </label>
-          <input type="text" className="input-box"></input>
+          <input
+            type="text"
+            onChange={(e) => setPhone(e.target.value)}
+          ></input>
         </div>
-        
-        <button>Submit</button>
+
+        <button onClick={handleSubmit}>Submit</button>
       </form>
     </div>
   );
